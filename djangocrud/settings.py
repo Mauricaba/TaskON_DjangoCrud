1863353ba5377cc47e86613542deb46b5b47b938
Mauricaba <mauricaba@gmail.com>
2023-11-06 14:28:33 -0300

    migracion de DB a postgreSQL

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'eqvdrvgase',
+        'HOST': '127.0.0.1', 
+        'PORT': '5433',
     }
 }
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+  titulo = models.CharField(max_length=100)
+  descripcion = models.TextField(blank=True)
+  created = models.DateTimeField(auto_now_add=True)
+  fechaterminada = models.DateTimeField(null=True)
+  prioridad = models.BooleanField(default=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE) 
