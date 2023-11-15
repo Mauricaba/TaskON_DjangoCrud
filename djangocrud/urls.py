56ee02244fc17628334a52bba48bc225dccfd135
Mauricaba <mauricaba@gmail.com>
2023-11-15 14:26:28 -0300

    cambios varios

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -24,7 +24,10 @@ urlpatterns = [
     path('register/', views.logueado, name='register'),
     path('tasks/', views.tasks, name='tasks'),
     path('tasks/create/', views.create_task, name='create_task'),
+    path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
     path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('tasks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
     path('logout/', views.signout, name='signout'),
     path('signin/', views.signin, name='signin'),
 
--- tasks/views.py
+++ tasks/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.db import IntegrityError
+from django.utils import timezone
 from .forms import TaskForm
 from .models import Task
 
 
 def home(request):
     return render(request, 'home.html')
-
 
 def logueado(request):
 
@@ -37,19 +38,58 @@ def logueado(request):
         'error': 'La contraseña no coincide.',
     })
 
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.fecha_terminada = timezone.now()
+        task.save()
+        return redirect('tasks')
 
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+    
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, fecha_terminada__isnull=True)
     return render(request, 'tasks.html',{
         'tasks' : tasks
     })
 
-def task_detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, 'task_detail.html' , {
-        'task' : task
-        })
+@login_required
+def tasks_completed(request):
+    tasks_completed = Task.objects.filter(user=request.user, fecha_terminada__isnull=False).order_by('-fecha_terminada')
+    return render(request, 'tasks.html',{
+        'tasks' : tasks_completed
+    })
 
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {
+            'task' : task,
+            'form' : form,
+            })
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+            'task' : task,
+            'form' : form,
+            'error' : "Introduccion de datos invalida al actualizar.",
+            })
+
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -68,7 +108,7 @@ def create_task(request):
                 'error' : 'Inserte datos válidos'
             })
 
-
+@login_required
 def signout (request):
     logout(request)
     return redirect('home')
