481d20c2675a0390beae80f374de0e1ac705e9c5
Mauricaba <mauricaba@gmail.com>
2023-11-30 10:28:36 -0300

    changes varios

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'djangocrud/staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/css/src'),)
 
 LOGIN_URL = '/signin'
 
