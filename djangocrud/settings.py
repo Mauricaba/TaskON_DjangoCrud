ba0cdeb818dab75df52d937e0a525826186b2524
Mauricaba <mauricaba@gmail.com>
2023-11-30 16:44:37 -0300

    tailwind bugfixes

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/css/src'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_URL = '/signin'
 
